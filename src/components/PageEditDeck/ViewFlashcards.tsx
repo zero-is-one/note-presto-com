a920486dc6fcb4dd7632e999ec1541e2a411fa3f
Luke NB <luke@0is1.com>
2024-01-29 16:23:45 -0500

    Edit mode for flashcards and modals

--- src/components/PageEditDeck/ViewFlashcards.tsx
+++ src/components/PageEditDeck/ViewFlashcards.tsx
@@ -1,60 +1,52 @@
 import { extensions } from "@/components/FlashcardTextEditor/FlashcardTextEditor";
 import { LayoutSimpleBar } from "@/components/LayoutSimpleBar/LayoutSimpleBar";
 import { useDeckContainer } from "@/hooks/useDeck";
+import { Flashcard } from "@/types";
 import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
-import { ActionIcon, Button, Group, Title, rem } from "@mantine/core";
-import { useListState } from "@mantine/hooks";
-import { generateJSON } from "@tiptap/html";
 import {
-  MdArrowBack,
-  MdDragIndicator,
-  MdEditNote,
-  MdOutlinePostAdd,
-} from "react-icons/md";
+  ActionIcon,
+  Badge,
+  Button,
+  Group,
+  Text,
+  Title,
+  rem,
+} from "@mantine/core";
+import { useListState } from "@mantine/hooks";
+import { modals } from "@mantine/modals";
+import { generateJSON } from "@tiptap/html";
+import { useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { MdArrowBack, MdDragIndicator, MdOutlinePostAdd } from "react-icons/md";
+import { RiDeleteBin2Fill, RiListSettingsFill } from "react-icons/ri";
+
 import { Link, useNavigate } from "react-router-dom";
 
 export function ViewFlashcards() {
   const navigate = useNavigate();
   const { deck, save: saveDeck } = useDeckContainer();
   const [state, handlers] = useListState(deck?.flashcards);
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const items = state.map((item, index) => (
-    <Draggable key={item.id} index={index} draggableId={item.id}>
-      {(provided, snapshot) => (
-        <Button.Group
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          mb={6}
-          style={getDragXAxisLockStyle({
-            ...provided.draggableProps.style,
-            ...snapshot,
-          })}
-        >
-          <Button
-            component={"div"}
-            {...provided.dragHandleProps}
-            variant={snapshot.isDragging ? "filled" : "light"}
-          >
-            <MdDragIndicator
-              style={{ width: rem(18), height: rem(18) }}
-              stroke={1.5}
-            />
-          </Button>
-
-          <Button
-            component={Link}
-            to={item.id}
-            w={"100%"}
-            variant="light"
-            rightSection={<MdEditNote fullWidth size={14} />}
-          >
-            {item.noteName} - {simplify(item.prompt)} -{" "}
-            {simplify(item.response)}
-          </Button>
-        </Button.Group>
-      )}
-    </Draggable>
-  ));
+  const openDeleteModal = ({ flashcard }: { flashcard: Flashcard }) =>
+    modals.openConfirmModal({
+      title: "Are you sure you want to delete this card?",
+      size: "sm",
+      radius: "md",
+      centered: true,
+      withCloseButton: false,
+      children: (
+        <Text size="sm">
+          Deleting this flashcard will remove it from the deck. This action
+          cannot be undone.
+        </Text>
+      ),
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      //onCancel: () => console.log("Cancel"),
+      onConfirm: () => {
+        handlers.remove(deck?.flashcards.indexOf(flashcard) || 0);
+      },
+    });
 
   return (
     <LayoutSimpleBar
@@ -75,31 +67,122 @@ export function ViewFlashcards() {
       }
     >
       <Group justify="space-between" align="flex-end" mb="sm">
-        <Title size={"h2"}>Flashcards</Title>
-        <Button
-          onClick={() => {
-            navigate("new");
-          }}
-          rightSection={<MdOutlinePostAdd />}
-          size="sm"
-        >
-          Add Flashcard
-        </Button>
-      </Group>
-      <DragDropContext
-        onDragEnd={({ destination, source }) =>
-          handlers.reorder({ from: source.index, to: destination?.index || 0 })
-        }
-      >
-        <Droppable droppableId="dnd-list" direction="vertical">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {items}
-              {provided.placeholder}
-            </div>
+        <Group>
+          <Title size={"h2"}>Flashcards</Title>
+          <Badge color="blue">{deck?.flashcards.length} cards</Badge>
+        </Group>
+        <Group>
+          {!isEditMode && (
+            <>
+              <Button
+                onClick={() => {
+                  navigate("new");
+                }}
+                leftSection={<MdOutlinePostAdd />}
+                size="sm"
+              >
+                Add Flashcard
+              </Button>{" "}
+              <ActionIcon
+                variant="filled"
+                aria-label="Settings"
+                size={"lg"}
+                onClick={() => {
+                  setIsEditMode(true);
+                }}
+              >
+                <RiListSettingsFill
+                  style={{ width: "70%", height: "70%" }}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+            </>
           )}
-        </Droppable>
-      </DragDropContext>
+
+          {isEditMode && (
+            <Button
+              color="green"
+              onClick={() => {
+                setIsEditMode(false);
+              }}
+              leftSection={<FaCheck />}
+              size="sm"
+            >
+              Done
+            </Button>
+          )}
+        </Group>
+      </Group>
+      {!isEditMode && (
+        <>
+          {state.map((item) => (
+            <Button.Group mb={6} key={item.id}>
+              <Button w={"100%"} variant="light" component={Link} to={item.id}>
+                {item.noteName} - {simplify(item.prompt)} -{" "}
+                {simplify(item.response)}
+              </Button>
+            </Button.Group>
+          ))}
+        </>
+      )}
+      {isEditMode && (
+        <DragDropContext
+          onDragEnd={({ destination, source }) =>
+            handlers.reorder({
+              from: source.index,
+              to: destination?.index || 0,
+            })
+          }
+        >
+          <Droppable droppableId="dnd-list" direction="vertical">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {state.map((item, index) => (
+                  <Draggable key={item.id} index={index} draggableId={item.id}>
+                    {(provided, snapshot) => (
+                      <Button.Group
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        mb={6}
+                        style={getDragXAxisLockStyle({
+                          ...provided.draggableProps.style,
+                          ...snapshot,
+                        })}
+                      >
+                        <Button
+                          component={"div"}
+                          {...provided.dragHandleProps}
+                          variant={snapshot.isDragging ? "filled" : "light"}
+                        >
+                          <MdDragIndicator
+                            style={{ width: rem(18), height: rem(18) }}
+                            stroke={1.5}
+                          />
+                        </Button>
+
+                        <Button w={"100%"} variant="light">
+                          {item.noteName} - {simplify(item.prompt)} -{" "}
+                          {simplify(item.response)}
+                        </Button>
+
+                        <Button
+                          color={"red"}
+                          onClick={() => {
+                            openDeleteModal({ flashcard: item });
+                          }}
+                        >
+                          <RiDeleteBin2Fill size={"1.5rem"} />
+                        </Button>
+                      </Button.Group>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      )}
     </LayoutSimpleBar>
   );
 }
@@ -126,14 +209,14 @@ function simplify(markdown: string) {
   const json = generateJSON(markdown, extensions) as Node;
   let result = "";
 
-  console.log(json);
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const recurse = (node: Node) => {
     if (node.type === "text") result += node.text;
 
     if (node.type === "sheetMusicNoteExtension")
-      result += `♫ ${node.attrs.noteName} `;
+      result += `♫ ${node.attrs.noteName}${
+        node.attrs.clef !== "treble" ? " " + node.attrs.clef : ""
+      }`;
 
     if (node.type === "svgEmbedExtension") result += `{SVG}`;
 
@@ -144,9 +227,3 @@ function simplify(markdown: string) {
 
   return "[" + result + "]";
 }
-
-// json.content.forEach((node: { type: string; attrs: Record<string, any> }) => {
-//   if (node.type === "sheetMusicNoteExtension") {
-//     result += `♫ ${node.attrs.noteName} `;
-//   }
-// });
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { Router } from "@/router.tsx";
 import { theme } from "@/theme.ts";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { ModalsProvider } from "@mantine/modals";
 import "@mantine/tiptap/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
       <MicrophoneProvider>
-        <Router />
+        <ModalsProvider>
+          <Router />
+        </ModalsProvider>
       </MicrophoneProvider>
     </MantineProvider>
   </React.StrictMode>,
